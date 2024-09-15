38592c42388453544296ff7bc4ab41e426dab67a
se-thato <thatoselepe80@gmail.com>
2024-09-15 20:11:58 +0000

    new commit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -20,3 +20,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -20,3 +20,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "posts")
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now_add= True)
