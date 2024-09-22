ca4684795ba08c3954b9c86965a40fd98b799ea4
thatoselepe80@gmail.com <thatoselepe80@gmail.com>
2024-09-22 23:36:50 +0200

    add changes

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+User = get_user_model().objects.create_user
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
