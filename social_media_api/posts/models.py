4b749db3d70e0dd3e33e4e5613254e84faed2f96
thatoselepe80@gmail.com <thatoselepe80@gmail.com>
2024-09-23 14:37:05 +0200

    first commit

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -16,8 +16,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
