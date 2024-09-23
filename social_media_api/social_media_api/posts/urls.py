5ab4b7f4f170857705370eed326e04ff649a7d9a
thatoselepe80@gmail.com <thatoselepe80@gmail.com>
2024-09-23 14:03:17 +0200

    first commit

--- social_media_api/social_media_api/posts/urls.py
+++ social_media_api/social_media_api/posts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path    
+from . import views
 from .views import PostListCreate, PostRetriveUpdateDestroy, CommentListCreate, CommentRetriveUpdateDestroy
 
 
@@ -7,4 +8,10 @@ urlpatterns = [
     path('post/<int:pk>/', PostRetriveUpdateDestroy.as_view(), name='update'),
     path('comment/', CommentListCreate.as_view(), name='comment' ),
     path('comment/<int:pk>/', CommentRetriveUpdateDestroy.as_view(), name='update-comment'),
+]
+
+
+urlpatterns = [
+    path('/follow/<int:user_id>/', views.follow_user, name='follow_user'),
+    path('/unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
 ]
--- social_media_api/social_media_api/posts/views.py
+++ social_media_api/social_media_api/posts/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import generics,status
 from rest_framework.response import Response
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from django.http import HttpResponseForbidden
+from django.contrib.auth.models import User
+
 
 class PostListCreate(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -23,7 +26,7 @@ class CommentListCreate(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = CommentSerializer
 
-    def delete(self, request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):  
         Comment.objects.all().delete
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -32,3 +35,30 @@ class CommentRetriveUpdateDestroy(generics.RetrieveUpdatedDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = CommentSerializer
     lookup_field = 'pk'
+
+
+    def follow_user(request, username):
+        #making sure the user is aunthenticated
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("You need to be logged in to follow other users")
+
+        #preventing the user to follow themselves
+        if request.user.username == username:
+            return HttpResponseForbidden("Opps!! you cannot follow yourself.")
+        
+        #getting the user to follow
+        user_to_follow = get_object_or_404(User, username=username)
+
+
+    def unfollow_user(request, username):
+        #making sure the user is aunthenticated
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("You need to be logged in to follow other users")
+        
+
+        #preventing a user from unfollowing themselves
+        if request.user.username == username:
+            return HttpResponseForbidden("Opps!! you cannot unfollow yourself.")
+
+        #get the user to be able to unfollow
+        user_to_unfollow = get_object_or_404(User, username=username)
