406ce939f3f5366bf1217e83ab13c794344b942a
se-thato <thatoselepe80@gmail.com>
2024-09-29 23:15:23 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -62,3 +62,43 @@ class CommentRetriveUpdateDestroy(generics.RetrieveUpdatedDestroyAPIView):
 
         #get the user to be able to unfollow
         user_to_unfollow = get_object_or_404(User, username=username)
+
+
+
+
+
+
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Post, Like
+from notifications.models import Notification
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def like_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
+    
+    if created:
+        # Create notification
+        Notification.objects.create(
+            recipient=post.author,
+            actor=request.user,
+            verb='liked your post',
+            target_content_type=ContentType.objects.get_for_model(post),
+            target_object_id=post.id
+        )
+        return JsonResponse({'message': 'Post liked'}, status=201)
+    else:
+        return JsonResponse({'message': 'You have already liked this post'}, status=400)
+
+@login_required
+def unlike_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    try:
+        like = Like.objects.get(user=request.user, post=post)
+        like.delete()
+        return JsonResponse({'message': 'Post unliked'}, status=200)
+    except Like.DoesNotExist:
+        return JsonResponse({'message': 'You have not liked this post'}, status=400)
